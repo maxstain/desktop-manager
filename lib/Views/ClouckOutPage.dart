59fc70cfe6c73e6d587ef78abe43c0e42c3abaa0
maxstain <firaschabchoub@hotmail.com>
2023-09-11 13:04:36 +0100

    [FIX]: Fixing some bugs

--- lib/Views/ClouckOutPage.dart
+++ lib/Views/ClouckOutPage.dart
@@ -21,95 +21,104 @@ class _ClockOutPageState extends State<ClockOutPage> {
         const TimeOfDay(hour: 15, minute: 00).hour - TimeOfDay.now().hour;
     late int minutes =
         const TimeOfDay(hour: 16, minute: 60).minute - TimeOfDay.now().minute;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Clock Out'),
-      ),
-      body: ListView(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: widget.isFree == false ? Colors.white : Colors.green,
-              border: Border.all(
-                color: Theme.of(context).colorScheme.secondary,
-                width: 2,
-              ),
-            ),
-            margin: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
-            ),
-            width: MediaQuery.of(context).size.width - offset,
-            height: 100,
-            child: DateTime.now().month == 7 || DateTime.now().month == 8
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        // Time until clocking out (6:00 PM)
-                        'Time until clocking out:',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      hours > 0
-                          ? Text(
-                              '$hours hours '
-                              '$minutes minutes',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            )
-                          : Text(
-                              '$minutes minutes',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                    ],
-                  )
-                : Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        // Time until clocking out (6:00 PM)
-                        'Time until clocking out:',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      hours > 0
-                          ? Text(
-                              '$hours hours '
-                              '$minutes minutes',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            )
-                          : Text(
-                              '$minutes minutes',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                    ],
-                  ),
+    return ListView(
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
           ),
-        ],
-      ),
+          child: const Text(
+            'Time Remaining',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: widget.isFree == false ? Colors.white : Colors.green,
+            border: Border.all(
+              color: Theme.of(context).colorScheme.secondary,
+              width: 2,
+            ),
+          ),
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          width: MediaQuery.of(context).size.width - offset,
+          height: 100,
+          child: DateTime.now().month == 7 || DateTime.now().month == 8
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      // Time until clocking out (6:00 PM)
+                      'Time until clocking out:',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    hours > 0
+                        ? Text(
+                            '$hours hours '
+                            '$minutes minutes',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          )
+                        : Text(
+                            '$minutes minutes',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                  ],
+                )
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      // Time until clocking out (6:00 PM)
+                      'Time until clocking out:',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    hours > 0
+                        ? Text(
+                            '$hours hours '
+                            '$minutes minutes',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          )
+                        : Text(
+                            '$minutes minutes',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                  ],
+                ),
+        ),
+      ],
     );
   }
 }
--- lib/Views/DaysRemainingPage.dart
+++ lib/Views/DaysRemainingPage.dart
@@ -16,55 +16,64 @@ class _DaysRemainingPageState extends State<DaysRemainingPage> {
   @override
   Widget build(BuildContext context) {
     double offset = 30;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Days Remaining'),
-      ),
-      body: ListView(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.black,
-            ),
-            margin: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
-            ),
-            width: MediaQuery.of(context).size.width - offset,
-            height: 100,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  // Time until clocking out (6:00 PM)
-                  'Slavery days:',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                // Days, months, and years until the 19th of December 2028
-                Text(
-                  '${DateTime(2028, 12, 19).difference(DateTime.now()).inDays} days, '
-                  '${DateTime(2028, 12, 19).difference(DateTime.now()).inHours % 24} hours, '
-                  '${DateTime(2028, 12, 19).difference(DateTime.now()).inMinutes % 60} minutes, '
-                  '${DateTime(2028, 12, 19).difference(DateTime.now()).inSeconds % 60} seconds',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
+    return ListView(
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          child: const Text(
+            'Days Remaining',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
           ),
-        ],
-      ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.black,
+          ),
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          width: MediaQuery.of(context).size.width - offset,
+          height: 100,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                // Time until clocking out (6:00 PM)
+                'Slavery days:',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              // Days, months, and years until the 19th of December 2028
+              Text(
+                '${DateTime(2028, 12, 19).difference(DateTime.now()).inDays} days, '
+                '${DateTime(2028, 12, 19).difference(DateTime.now()).inHours % 24} hours, '
+                '${DateTime(2028, 12, 19).difference(DateTime.now()).inMinutes % 60} minutes, '
+                '${DateTime(2028, 12, 19).difference(DateTime.now()).inSeconds % 60} seconds',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
