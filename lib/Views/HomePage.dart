0139dfe509e7d9e90eb600db614a024bf573f92c
maxstain <firaschabchoub@hotmail.com>
2023-10-02 09:27:59 +0100

    [REFACTOR]: Refactoring

--- lib/Views/HomePage.dart
+++ lib/Views/HomePage.dart
@@ -126,173 +126,181 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        widget.isLastDay
-            ? Container(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 10,
-                ),
-                child: const Text(
-                  'TODAY IS YOUR LAST DAY!!!!',
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {});
+        },
+        child: const Icon(Icons.refresh),
+      ),
+      body: ListView(
+        children: [
+          widget.isLastDay
+              ? Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 10,
+                  ),
+                  child: const Text(
+                    'TODAY IS YOUR LAST DAY!!!!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              : Container(),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: Theme.of(context).colorScheme.secondary,
+                width: 2,
+              ),
+            ),
+            margin: const EdgeInsets.only(
+              right: 20,
+              left: 20,
+              bottom: 10,
+            ),
+            width: MediaQuery.of(context).size.width - offset,
+            height: 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'Today\'s date and time:',
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: 30,
+                    fontSize: 20,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              )
-            : Container(),
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: Theme.of(context).colorScheme.secondary,
-              width: 2,
+                Text(
+                  '${DateTime.now().toString().substring(0, 10)} ${DateTime.now().toString().substring(11, 16)}',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
           ),
-          margin: const EdgeInsets.only(
-            right: 20,
-            left: 20,
-            bottom: 10,
-          ),
-          width: MediaQuery.of(context).size.width - offset,
-          height: 100,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                'Today\'s date and time:',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: SharedData().isFree == false ? Colors.white : Colors.green,
+              border: Border.all(
+                color: Theme.of(context).colorScheme.secondary,
+                width: 2,
               ),
-              Text(
-                '${DateTime.now().toString().substring(0, 10)} ${DateTime.now().toString().substring(11, 16)}',
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+            ),
+            margin: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            width: MediaQuery.of(context).size.width - offset,
+            height: 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  // Time until clocking out (6:00 PM)
+                  'Time until clocking out:',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    overflow: TextOverflow.ellipsis,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: SharedData().isFree == false ? Colors.white : Colors.green,
-            border: Border.all(
-              color: Theme.of(context).colorScheme.secondary,
-              width: 2,
+                hours > 0
+                    ? Text(
+                        '$hours hours '
+                        '$minutes minutes',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      )
+                    : Text(
+                        '$minutes minutes',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+              ],
             ),
           ),
-          margin: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 10,
-          ),
-          width: MediaQuery.of(context).size.width - offset,
-          height: 100,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                // Time until clocking out (6:00 PM)
-                'Time until clocking out:',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  overflow: TextOverflow.ellipsis,
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.black,
+            ),
+            margin: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            width: MediaQuery.of(context).size.width - offset,
+            height: 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  // Time until clocking out (6:00 PM)
+                  'Slavery days:',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              hours > 0
-                  ? Text(
-                      '$hours hours '
-                      '$minutes minutes',
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    )
-                  : Text(
-                      '$minutes minutes',
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-            ],
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.black,
-          ),
-          margin: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 10,
-          ),
-          width: MediaQuery.of(context).size.width - offset,
-          height: 100,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                // Time until clocking out (6:00 PM)
-                'Slavery days:',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+                // Days, months, and years until the 19th of December 2028
+                Text(
+                  '${DateTime(2028, 12, 19).difference(DateTime.now()).inDays} days, '
+                  '${DateTime(2028, 12, 19).difference(DateTime.now()).inHours % 24} hours, '
+                  '${DateTime(2028, 12, 19).difference(DateTime.now()).inMinutes % 60} minutes, '
+                  '${DateTime(2028, 12, 19).difference(DateTime.now()).inSeconds % 60} seconds',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              // Days, months, and years until the 19th of December 2028
-              Text(
-                '${DateTime(2028, 12, 19).difference(DateTime.now()).inDays} days, '
-                '${DateTime(2028, 12, 19).difference(DateTime.now()).inHours % 24} hours, '
-                '${DateTime(2028, 12, 19).difference(DateTime.now()).inMinutes % 60} minutes, '
-                '${DateTime(2028, 12, 19).difference(DateTime.now()).inSeconds % 60} seconds',
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        Lottie.asset(
-          widget.isFree == false
-              ? 'assets/lottie/Black1.json'
-              : 'assets/lottie/BearLottie.json',
-          height: 300,
-          width: 300,
-          options: LottieOptions(enableMergePaths: true),
-        ),
-        Text(
-          widget.isFree == false && widget.isLastDay == false
-              ? 'You are currently not free'
-              : widget.isFree && widget.isLastDay
-                  ? 'YOU ARE FINALLY FREE!!!'
-                  : 'YOU ARE FREE FOR THE REST OF THE DAY!!',
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
+          Lottie.asset(
+            widget.isFree == false
+                ? 'assets/lottie/Black1.json'
+                : 'assets/lottie/BearLottie.json',
+            height: 300,
+            width: 300,
+            options: LottieOptions(enableMergePaths: true),
           ),
-        ),
-      ],
+          Text(
+            widget.isFree == false && widget.isLastDay == false
+                ? 'You are currently not free'
+                : widget.isFree && widget.isLastDay
+                    ? 'YOU ARE FINALLY FREE!!!'
+                    : 'YOU ARE FREE FOR THE REST OF THE DAY!!',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
